47aba0898e728d4d698ef0c53fc58b9a35e8ea02
Konstantinos Boulis <costas.boulis@gmail.com>
2012-02-16 17:29:28 +0200

    Normalize description for FashionPlus products

--- catalog/src/main/java/com/cleargist/catalog/dao/FashionPlusCatalog.java
+++ catalog/src/main/java/com/cleargist/catalog/dao/FashionPlusCatalog.java
@@ -6,16 +6,36 @@ import java.util.Locale;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.cleargist.catalog.entity.jaxb.Catalog;
 
+/*
+ * Assumes that the URL is a UUID for products. 
+ */
 public class FashionPlusCatalog extends CatalogDAOImpl {
 	private Logger logger = LoggerFactory.getLogger(getClass());
 	private static String USER_AGENT = "ClearGist_bot";
 	private static final Locale locale = new Locale("el", "GR"); 
+	
+	private String removeSpecialChars(String in) {
+		String out = in.replaceAll("\\d+\\.\\d+", "NUMBER");
+		out = out.replaceAll("\\d+", "NUMBER");
+		out = out.toLowerCase(locale);
+		out = out.replaceAll("[\\.,\\(\\)\\?;!:\\[\\]\\{\\}\"%&\\*'\\+/>-]", "");
+		out = out.replace('ά', 'α');
+		out = out.replace('ό', 'ο');
+		out = out.replace('ή', 'η');
+		out = out.replace('ώ', 'ω');
+		out = out.replace('ύ', 'υ');
+		out = out.replace('έ', 'ε');
+		out = out.replace('ί', 'ι');
+		out = out.replaceAll("\\s+", " ");
+		out = out.trim();
+		
+		return out;
+	}
 	
 	public void addProduct(String url, String tenantID) throws Exception {
 		// Check if the product already exists
@@ -38,11 +58,14 @@ public class FashionPlusCatalog extends CatalogDAOImpl {
 		product.setCategory(category);
 		
 		String description = productDetails.getElementsByAttributeValue("id", "pDescription").get(0).text();
-		product.setDescription(description);
+		product.setDescription(removeSpecialChars(description));
 		
 		Element imageDetails = doc.select("[class=product-image]").get(0);
 		String image = imageDetails.attr("src");
 		product.setImage(image);
+		
+		product.setInstock("Y");
+		
 		
 		addProduct(product, "", tenantID);
 	}
