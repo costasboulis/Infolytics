f3497ebccfb5d61c691c8732511269f87bc2d08f
Konstantinos Boulis <costas.boulis@gmail.com>
2012-03-16 12:27:59 +0200

    Deleted deprecated files

--- catalog/src/main/java/com/cleargist/catalog/deals/scrape/GoldenDealsScraper.java
+++ catalog/src/main/java/com/cleargist/catalog/deals/scrape/GoldenDealsScraper.java
@@ -2,8 +2,6 @@ package com.cleargist.catalog.deals.scrape;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -20,15 +18,8 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Locale;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
 import javax.xml.datatype.DatatypeFactory;
-import javax.xml.validation.Schema;
-import javax.xml.validation.SchemaFactory;
 
-
-import com.cleargist.catalog.dao.MyValidationEventHandler;
 import com.cleargist.catalog.deals.entity.jaxb.AddressListType;
 import com.cleargist.catalog.deals.entity.jaxb.Collection;
 import com.cleargist.catalog.deals.entity.jaxb.DealType;
@@ -37,7 +28,6 @@ import org.apache.log4j.Logger;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.jsoup.Jsoup;
 
 public class GoldenDealsScraper extends Scraper {
 	private static final Locale locale = new Locale("el", "GR"); 
@@ -52,7 +42,7 @@ public class GoldenDealsScraper extends Scraper {
 	private GreekOnePersonCouponResolver onePersonCouponResolver;
 	private GreekHasExtraDiscountsResolver hasExtraDiscountsResolver;
 	private GreekHasOptionsResolver hasOptionsResolver;
-	
+
 	public GoldenDealsScraper() {
 		this.formatter = new SimpleDateFormat(DATE_PATTERN);
 		this.couponRedemptionResolver = new GreekCouponRedemptionResolver();
@@ -64,7 +54,7 @@ public class GoldenDealsScraper extends Scraper {
 		this.hasExtraDiscountsResolver = new GreekHasExtraDiscountsResolver();
 		this.hasOptionsResolver = new GreekHasOptionsResolver();
 	}
-	
+
 	private static String readAll(Reader rd) throws IOException {
 		StringBuilder sb = new StringBuilder();
 	    int cp;
@@ -85,47 +75,47 @@ public class GoldenDealsScraper extends Scraper {
 			is.close();
 		}
 	}
-	
+
 
 	public DealType scrape(String url) throws Exception {
 		DealType deal = new DealType();
 		JSONObject json = readJsonFromUrl(url);
-		
+
 		String status = json.getString("status");
 		if (!status.equals("OK")) {
 			logger.warn("Could not get data from URL " + url);
 			return deal;
 		}
-		
+
 		JSONArray array = json.getJSONArray("resultData");
 		JSONObject dealJson = null;
 		String dealURLId = new URL(url).getPath().replaceAll("\\.js", "").replaceAll("/deals/json/detailed/", "");
-		
+
 		int length = array.length();
 		for (int i = 0; i < length; i ++) {
 			String dealURL = array.getJSONObject(i).getString("url");
-			
+
 			if (dealURL.equals(dealURLId)) {
 				dealJson = array.getJSONObject(i);
-				
+
 				break;
 			}
-			
+
 		}
-		
+
 		if (dealJson == null) {
 			logger.warn("Could not locate deal for url " + url);
 			return deal;
 		}
-		
-		
+
+
 //		for (String name : JSONObject.getNames(dealJson)) {
 //			System.out.println(name);
 //		}
-		
-		
+
+
 		String merchantName = dealJson.getString("merchantName");
-		
+
 		String dueDateString = dealJson.getString("dueDate");
 		Date dueDate = this.formatter.parse(dueDateString);
 		String startDateString = dealJson.getString("startDate");
@@ -142,7 +132,7 @@ public class GoldenDealsScraper extends Scraper {
 			nonDiscountString = dealJson.getString("nonDiscountString");
 		}
 		catch (Exception ex) {
-			
+
 		}
 		JSONObject belongsToJson = dealJson.getJSONObject("belongsTo");
 		List<String> categories = deal.getSiteCategories();
@@ -152,7 +142,7 @@ public class GoldenDealsScraper extends Scraper {
 			}
 			categories.add(category);
 		}
-		
+
 		String[] descriptionParts = description.split("<h2>");
 		String allTerms = html2text(descriptionParts[0]).toLowerCase(locale)
 		.replaceAll("<\\\\/strong>", "")
@@ -161,8 +151,8 @@ public class GoldenDealsScraper extends Scraper {
 		.replaceAll("<\\\\/b>", "")
 		.replaceAll("\\[", "");
 		String[] terms = allTerms.split("\\\\r\\\\n");
-		
-		
+
+
 		deal.setSiteId("Golden Deals");
 		deal.setSiteCity(cityTag);
 		deal.setSiteCountry(locale.getCountry());
@@ -171,22 +161,22 @@ public class GoldenDealsScraper extends Scraper {
 		deal.setInitialPrice(new BigDecimal(initialPrice.toString()));
 		deal.setBusinessName(merchantName);
 		AddressListType address = GreekAddressResolver.resolve(location.replaceAll("GoldenTip.+", ""));
-		
+
 		deal.setBusinessAddress(address);
-		
+
 		// Calendar conversions
 		GregorianCalendar gc = new GregorianCalendar();
         gc.setTimeInMillis(startDate.getTime());
         DatatypeFactory df = DatatypeFactory.newInstance();
         
 		deal.setCouponPurchaseStartingDate(df.newXMLGregorianCalendar(gc));
-		
+
 		gc.setTimeInMillis(dueDate.getTime());
 		deal.setCouponPurchaseEndDate(df.newXMLGregorianCalendar(gc));
-		
+
 		deal.setDealDescription(description);
 		deal.setDealTitle(title);
-		
+
 		// Process the date terms
 		String dateTerms = null;
 		for (String term : terms) {
@@ -200,8 +190,8 @@ public class GoldenDealsScraper extends Scraper {
 			logger.error("Could not determine date terms line");
 			return deal;
 		}
-		
-		
+
+
 		List<Date> couponRedemptionDates = couponRedemptionResolver.resolve(dateTerms, dueDate.getYear());
 		if (couponRedemptionDates.size() > 0) {
 			if (couponRedemptionDates.get(0).getYear() == 1999) {
@@ -209,37 +199,37 @@ public class GoldenDealsScraper extends Scraper {
 			}
 			GregorianCalendar gcTmp = new GregorianCalendar();
 			gcTmp.setTimeInMillis(couponRedemptionDates.get(0).getTime());
-			
+
 			deal.setCouponRedemptionStartingDate(df.newXMLGregorianCalendar(gcTmp));
-			
+
 			if (couponRedemptionDates.size() > 1) {
 				gcTmp.setTimeInMillis(couponRedemptionDates.get(1).getTime());
-				
+
 				deal.setCouponRedemptionEndDate(df.newXMLGregorianCalendar(gcTmp));
 			}
 		}
-		
+
 		// Process the valid dates
 		List<String> validDates = deal.getValidDates();
 		List<String> tmpValidDates = validDatesResolver.resolve(dateTerms);
 		if (tmpValidDates != null && tmpValidDates.size() != 0) {
 			validDates.addAll(tmpValidDates);
 		}
-		
-		
+
+
 		// Process the blocker dates
 		boolean hasBlockerDates = blockerDatesResolver.resolve(dateTerms);
 		deal.setHasBlockerDates(hasBlockerDates);
-		
-		
+
+
 		// Process the phone reservation
 		deal.setRequiresPhoneReservation(this.phoneReservationResolver.resolve(allTerms));
-		
-		
+
+
 		// Process the coupons sold
 		String urlCoupons = "http://dynamic.goldendeals.gr/deals/json/dynamic?city=" + deal.getSiteCity() + "&dealUrl=" + deal.getDealId();
 		JSONObject jsonCoupons = readJsonFromUrl(urlCoupons);
-		
+
 		String statusCoupons = jsonCoupons.getString("status");
 		if (!statusCoupons.equals("OK")) {
 			logger.warn("Could not get data from URL " + urlCoupons);
@@ -248,10 +238,10 @@ public class GoldenDealsScraper extends Scraper {
 			JSONObject dealJsonCoupons = jsonCoupons.getJSONObject("resultData").getJSONObject(deal.getDealId());
 			Integer numCoupons = dealJsonCoupons.getInt("sales");
 			Integer tippedSales = dealJsonCoupons.getInt("tippedSales");
-			
+
 			deal.setNumberOfCouponsSold(new BigInteger(numCoupons.toString()));
 			deal.setMinCouponsForActivation(new BigInteger(tippedSales.toString()));
-			
+
 			try {
 				JSONArray variations = dealJsonCoupons.getJSONArray("variations");
 				deal.setHasMultiplePrices(true);
@@ -259,36 +249,36 @@ public class GoldenDealsScraper extends Scraper {
 			catch (Exception ex) {
 				deal.setHasMultiplePrices(false);
 			}
-			
-			
+
+
 		}
-		
+
 		// Process the max coupons per user
 		deal.setMaxCouponsPerPerson(new BigInteger(Integer.toString(this.maxCouponsPerPersonResolver.resolve(allTerms))));
-		
+
 		// Process has more to pay
 		deal.setRequiresMoreToPay(nonDiscountString == null || nonDiscountString.isEmpty() ? false : true);
-		
-		
+
+
 		// Process one person coupon
 		deal.setIsOnePersonCoupon(this.onePersonCouponResolver.resolve(allTerms));
-		
+
 		// Process has extra discounts
 		deal.setHasExtraDiscounts(this.hasExtraDiscountsResolver.resolve(allTerms));
-		
+
 		// Process has options
 		deal.setHasOptions(this.hasOptionsResolver.resolve(allTerms));
-		
+
 		return deal;
 	}
-	
-	
-	
+
+
+
 	public static void main(String[] argv) {
 		String dealsFilenameString = "c:\\Users\\kboulis\\goldenDeals.txt";
 //		String dealsFilenameString = "c:\\Users\\kboulis\\goldenDealsSmall.txt";
 		GoldenDealsScraper scraper = new GoldenDealsScraper();
-		
+
 		File dealsFile = new File(dealsFilenameString);
 		List<String> deals = new LinkedList<String>();
 		try {
@@ -303,7 +293,7 @@ public class GoldenDealsScraper extends Scraper {
 			System.err.println("Cannot load deals list");
 			System.exit(-1);
 		}
-		
+
 		Collection collection = null;
 		try {
 			collection = scraper.scrape(deals);
@@ -312,13 +302,13 @@ public class GoldenDealsScraper extends Scraper {
 			System.err.println("Cannot scrape url");
 			System.exit(-1);
 		}
-		
+
 		try {
 			scraper.marshall(collection, new File("C:\\Users\\kboulis\\deals.xsd"), new File("C:\\Users\\kboulis\\Infolytics\\catalog\\goldenDeals.xml"));
 		}
 		catch (Exception ex) {
 			System.exit(-1);
 		}
-		
+
 	}
 }
